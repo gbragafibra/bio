5ec57163d5e53816e5d970a747ab84f29390019f
gbragafibra <gbragafibra@protonmail.com>
2024-09-15 17:50:50 +0100

    +testing procedure

--- Net.py
+++ Net.py
@@ -60,35 +60,68 @@ class Net:
 		#Don't want T < 0
 		self.T = np.maximum(0, self.T)
 
-	def train(self, X, y, η, λ, epochs, plot = False):
-		losses = []
-		y_ = []
+	def run(self, X, y, η, λ, epochs,
+	train_ω, test_ω, plot = False):
+		#Splitting sets
+		idxs = np.random.permutation(X.shape[0])
+		train_size = int(X.shape[0] * train_ω)
+		#test_size = int(X.shape[0] * test_ω)
+
+		train_idxs = idxs[train_size:]
+		test_idxs = idxs[:train_size]
+		X_train, y_train = X[train_idxs], y[train_idxs]
+		X_test, y_test = X[test_idxs], y[test_idxs]
+
+
+		train_losses = []
+		train_y = []
+		test_losses = []
+		test_y = []
 		for epoch in range(epochs):
-			Loss = 0
-			for i in range(X.shape[0]):
-				x_i = X[i].reshape(-1, 1)
-				y_i = y[i].reshape(-1, 1)
+			Loss_train = 0
+			Loss_test = 0
+			#train
+			for i in range(X_train.shape[0]):
+				x_i = X_train[i].reshape(-1, 1)
+				y_i = y_train[i].reshape(-1, 1)
 
 				y_pred = self.forward(x_i)
 				if epoch == epochs - 1:
-					y_.append(y_pred.item())
-				L = se(y_i, y_pred)
-				Loss += L
+					train_y.append(y_pred.item())
+				L_train = se(y_i, y_pred)
+				Loss_train += L_train
 
-				self.update_T(L)
+				self.update_T(L_train)
 				self.update(η, λ)
+			#test
+			for i in range(X_test.shape[0]):
+				x_i = X_test[i].reshape(-1, 1)
+				y_i = y_test[i].reshape(-1, 1)
 
-			μ_loss = Loss / X.shape[0]
-			losses.append(μ_loss.item())
-			print(f"Epoch {epoch + 1}/{epochs}, Loss: {μ_loss.item():.4f}")
+				y_pred = self.forward(x_i)
+				if epoch == epochs - 1:
+					test_y.append(y_pred.item())
+				L_test = se(y_i, y_pred)
+				Loss_test += L_test
 
-		y_ = np.array(y_).reshape(-1, 1)
-		Acc = np.sum(np.where(np.where(y_ > 0.5, 1, 0) == y, 1, 0), axis = 0)/X.shape[0]
-		print(f"Training Accuracy: {Acc.item()}")
+			μ_loss_train = Loss_train / X_train.shape[0]
+			μ_loss_test = Loss_test / X_test.shape[0]
+			train_losses.append(μ_loss_train.item())
+			test_losses.append(μ_loss_test.item())
+			print(f"Epoch {epoch + 1}/{epochs}, Training Loss: {μ_loss_train.item():.4f},\
+				Testing Loss: {μ_loss_test.item():.4f}")
+
+		train_y = np.array(train_y).reshape(-1, 1)
+		test_y = np.array(test_y).reshape(-1, 1)
+		Acc_train = np.sum(np.where(np.where(train_y > 0.5, 1, 0) == y_train, 1, 0), axis = 0)/X_train.shape[0]
+		Acc_test = np.sum(np.where(np.where(test_y > 0.5, 1, 0) == y_test, 1, 0), axis = 0)/X_test.shape[0]
+		print(f"Training Accuracy: {Acc_train.item()}, Testing Accuracy: {Acc_test.item()}")
 		if plot:
-			plt.plot(range(1, epochs + 1), losses, "k")
+			plt.plot(range(1, epochs + 1), train_losses, "k", label = "Train Loss")
+			plt.plot(range(1, epochs + 1), test_losses, "r", label = "Test Loss")
 			plt.xlabel("Epoch")
 			plt.ylabel("Loss")
+			plt.legend()
 			plt.show()
 
 def change_ε(x_pre, x_pro):
@@ -125,8 +158,8 @@ def sigmoid(x):
 if __name__ == "__main__":
 	np.random.seed(37)
 	N = 10
-	X = np.array([np.random.randint(0, 2, size=N).reshape(1, -1) for _ in range(20)])
-	y = np.array([np.random.randint(0, 2) for _ in range(20)]).reshape(-1, 1)
+	X = np.array([np.random.randint(0, 2, size=N).reshape(1, -1) for _ in range(100)])
+	y = np.array([np.random.randint(0, 2) for _ in range(100)]).reshape(-1, 1)
 	layer_sizes = [N, 20, 1]
 	T = 50
 	γ = 0.95
@@ -136,4 +169,4 @@ if __name__ == "__main__":
 	η = 0.1
 	λ = 0.1
 	epochs = 200
-	net.train(X, y, η, λ, epochs, plot = True)
+	net.run(X, y, η, λ, epochs, 0.7, 0.3, plot = True)
